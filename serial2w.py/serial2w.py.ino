22bbb38bcd1ecebace031f23244df4f5a6ffacdd
rsegoly <roni.segoly@gmail.com>
2015-10-01 07:50:55 +0300

    Arduino code can now receive command with two parameter (delimited by comma (string,float) The pair will be sent to processing function Raspberry python knows how to send the commands

--- serial2w.py/serial2w.py.ino
+++ serial2w.py/serial2w.py.ino
@@ -1,14 +1,18 @@
-// Version 0.1   
+// Version 0.1
 // Roni Segoly roni@yozma.net
 // Date       : 26/9/15
 //https://github.com/bblanchon/ArduinoJson/blob/master/examples/JsonGeneratorExample/JsonGeneratorExample.ino
 #include <ArduinoJson.h>
+#include <Servo.h>
 StaticJsonBuffer<200> jsonBuffer;
 JsonObject& root = jsonBuffer.createObject();
-
+String received_message="";
+float received_value =0.0;
+Servo clawServo;
 void setup() 
 {
   Serial.begin(9600); // set the baud rate
+  clawServo.attach(8);
   Serial.println("Ready"); // print "Ready" once
 }
 void loop()
@@ -33,15 +37,33 @@ void loop()
     //put an end to the string
     str1[i]='\0';
     //add the sent command to json , just for debug
-    root["command"]=str1;
+    parse_message(str1);
+    root["command"]=received_message;
     //send back
     root.printTo(Serial);
+    process_message(received_message, received_value);
     //force readline on python side
     Serial.println("\n");
-    //clear the string
-    for (i=0;i<80;i++)
-      str1[0]='\0';
+    
   
   }
   delay(5000);
+}
+
+void process_message(String command, float value)
+{
+ //do somthing
+  
+  
+}
+
+void parse_message(String myStr)
+
+{
+  String myString=myStr;
+  int commaIndex = myString.indexOf(',');
+  received_message= myString.substring(0, commaIndex);
+  String valueString = myString.substring(commaIndex+1, myString.length());
+  received_value = valueString.toFloat();
+ 
 }
